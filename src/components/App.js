165300f3c8e0404c114ed39c15ad31b7e08038dc
Ana Martinez Fernandez <anamartinezf6@gmail.com>
2020-11-12 09:37:04 +0100

    Add PokeList component

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,23 @@
 import React from "react";
 import '../stylesheet/index.scss';
-
+import pokemonsFromApi from "../data/pokemon.json"; 
+import PokeList from "./PokeList";
 
 
 class App extends React.Component {
-  
+  constructor(props){
+    super(props);
+    this.state = {
+      data: pokemonsFromApi
+    }
+  }
+
+
   render(){
     return (
       < >
-        
+        <h1 className="page__title">Mi lista de pokemon</h1>
+        <PokeList dataList={this.state.data}/>
       </>
     );
   }
---
+++ src/components/PokeList.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+
+class PokeList extends React.Component {
+  render(){
+    return 
+   
+  };
+}
+
+
+
+export default PokeList;
+
